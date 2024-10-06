25daeade9db591ae23e2c08b7f1269ff79b9baa0
divy-11 <149963660+divy-11@users.noreply.github.com>
2024-10-06 19:55:47 +0530

    day1

--- apps/user/tailwind.config.js
+++ apps/user/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
