37cfcc1a7006adb59e7e57425752977749ad3538
divy-11 <149963660+divy-11@users.noreply.github.com>
2024-10-08 20:06:36 +0530

    try n catch in backend

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,11 +3,16 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
-    if (session.user) {
-        return NextResponse.json({
-            user: session.user
-        })
+    try {
+        const session = await getServerSession(authOptions);
+        if (session.user) {
+            return NextResponse.json({
+                user: session.user
+            })
+        }
+    }
+    catch(e){
+        console.log(e)
     }
     return NextResponse.json({
         message: "You are not logged in"
